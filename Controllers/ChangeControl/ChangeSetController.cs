24db74fc89583838c96402d2c44e57c9da40520e
Zahid <thezaza102@gmail.com>
2018-06-19 12:07:51 +1000

    stuff

--- Controllers/ChangeControl/ChangeSetController.cs
+++ Controllers/ChangeControl/ChangeSetController.cs
@@ -31,7 +31,12 @@ namespace RDMdotNet.Controllers
         [HttpPost]
         public IActionResult Post([FromBody]dynamic value)
         {
-            return StatusCode(201);
+            string postedData = value.ToString();
+            ChangeSet sysData = JsonConvert.DeserializeObject<ChangeSet>(postedData);
+            sysData.ID = Guid.NewGuid().ToString();
+            js.Add(sysData);
+            js.SaveChanges();
+            return StatusCode(201, sysData);
             
         }
 
@@ -39,7 +44,18 @@ namespace RDMdotNet.Controllers
         [HttpPut("{id}")]
         public IActionResult Put(string id, [FromBody]dynamic value)
         {
-            return StatusCode(201);
+            ChangeSet curData = js.Single<ChangeSet>(id);
+            string postedData = value.ToString();
+            ChangeSet newData = JsonConvert.DeserializeObject<ChangeSet>(postedData);
+            newData.ID = curData.ID;
+            js.Add(new Archive(curData, Reason.Update));            
+            js.Remove(curData);
+            if (newData.Active)
+            {
+                js.Add(newData);
+            }
+            js.SaveChanges();
+            return StatusCode(202); 
             
         }
 
